ec8b7e957c5a437d2fa2b4c4d367f0800d9d5aae
fadedrecallrune <fadedrecallrune@gmail.com>
2018-07-27 15:55:39 +0300

    tiny images

--- src/components/home/Prices.js
+++ src/components/home/Prices.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./Home.css"
 
-import backimage from '../../images/1.jpeg'
 import FirstPriceCard from './price-cards/FirstPriceCard';
 import SecondPriceCard from './price-cards/SecondPriceCard';
 
